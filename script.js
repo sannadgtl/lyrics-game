51fb919260df9606641e66007cee7f73a4a482ba
sans <sannadgtl@gmail.com>
2024-07-02 09:02:16 +0100

    getting elements in js

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const totalScore = document.querySelector(".total-score");
+const lyric = document.querySelector(".lyric");
+const startBtn = document.querySelector(".start-btn");
+const answerBtns = document.querySelectorAll(".answer");
+
+let scores;
