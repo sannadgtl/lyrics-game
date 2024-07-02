35dd11b00495f32353ac4813feafa28a11b1d416
sans <sannadgtl@gmail.com>
2024-07-02 10:56:38 +0100

    v1.0

--- script.js
+++ script.js
@@ -2,40 +2,58 @@
 
 const totalScore = document.querySelector(".total-score");
 const lyric = document.querySelector(".lyric");
-const startBtn = document.querySelector(".start-btn");
 const nextBtn = document.querySelector(".next-btn");
+const checkBtn = document.querySelector(".check-btn");
 const answerBtns = document.querySelectorAll(".answer");
+const answerComment = document.querySelector(".comment");
 
 let lyrics = [
   {
     lyric: "test1",
-    artist: 1,
+    artist: "1",
   },
   {
     lyric: "test2",
-    artist: 2,
+    artist: "2",
   },
   {
     lyric: "test3",
-    artist: 3,
+    artist: "3",
   },
 ];
-let scores;
 
-startBtn.addEventListener("click", function () {
-  startBtn.classList.toggle("hidden");
-  nextBtn.classList.toggle("hidden");
-  scores = 0;
-  for (let i = 0; i < lyrics.length; i++) {
+let scores = 0;
+let i = 0;
+let selectedAnswer;
+lyric.textContent = lyrics[i].lyric;
+
+answerBtns.forEach((btn) => {
+  btn.addEventListener("change", function () {
+    selectedAnswer = btn.value;
+    console.log(selectedAnswer);
+  });
+});
+
+checkBtn.addEventListener("click", function () {
+  if (selectedAnswer === lyrics[i].artist) {
+    scores += 1;
+    totalScore.textContent = scores;
+    console.log(scores);
+    answerComment.textContent = "correct answer";
+  } else {
+    answerComment.textContent = "wrong answer";
+  }
+});
+
+nextBtn.addEventListener("click", function () {
+  i++;
+  if (i < lyrics.length) {
     lyric.textContent = lyrics[i].lyric;
-
-    if (answerBtns.textContent === lyrics[i].artist) {
-      scores += 1;
-      totalScore.textContent = scores;
-    } else {
-      scores += 0;
-      totalScore.textContent = scores;
-    }
-    // click on "next" button
+    selectedAnswer = undefined;
+    answerBtns.forEach((btn) => {
+      btn.checked = false;
+    });
+  } else {
+    lyric.textContent = "game over";
   }
 });
